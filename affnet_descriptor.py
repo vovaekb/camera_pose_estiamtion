1c33fca39fae43a29910bab952ea04bc54cbac5c
Vladimir Privalov <vovaprivalov@gmail.com>
2023-10-01 12:50:19 +0400

    Refactor code

--- affnet_descriptor.py
+++ affnet_descriptor.py
@@ -1,41 +1,50 @@
 import numpy as np
 import os
 import cv2
-import json 
-import time
 import torch
-import argparse
-import kornia as K
-import pydegensac
 import kornia.feature as KF
-import matplotlib.pyplot as plt
-from kornia.feature import laf_from_center_scale_ori as get_laf
-from copy import deepcopy
 from extract_patches.core import extract_patches
 
-def extimate_affine_shape(kpts, img, affnet, dev = torch.device('cpu'), ellipse=False):
+
+def extimate_affine_shape(kpts,img, affnet, dev = torch.device('cpu'), ellipse=False):
     affnet = affnet.to(dev)
     affnet.eval()
-    patches = np.array(extract_patches(kpts,
-                                       cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),
-                                       32, 12., 'cv2')).astype(np.float32)
+    patches = np.array(
+        extract_patches(
+            kpts, cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), 32, 12., 'cv2'
+        )
+    ).astype(np.float32)
     bs = 128
     aff = np.zeros((len(patches), 3))
     for i in range(0, len(patches), bs):
-        data_a = torch.from_numpy(patches[i:min(i + bs, len(patches)),  :, :]).unsqueeze(1).to(dev)
+        data_a = torch.from_numpy(
+            patches[i:min(i + bs, len(patches)),  :, :]
+        ).unsqueeze(1).to(dev)
         with torch.no_grad():
             out_a = affnet(data_a)
             aff[i:i + bs] = out_a.view(-1, 3).cpu().detach().numpy()
     aff = torch.from_numpy(aff).to(dev)
     if ellipse:
         aff = aff.unsqueeze(1)
-        laf = KF.ellipse_to_laf(torch.cat([torch.zeros_like(aff[...,:2]),aff], dim=2))
+        laf = KF.ellipse_to_laf(
+            torch.cat([torch.zeros_like(aff[...,:2]),aff], dim=2)
+        )
     else:
-        aff2 = torch.cat([aff[:,0:1], torch.zeros_like(aff[:,0:1]), aff[:,1:2], aff[:,2:3]], dim=1).reshape(-1,2,2)
-        laf = torch.cat([aff2,torch.zeros_like(aff2[:,:, 0:1])],dim=2).unsqueeze(1)
+        aff2 = torch.cat([
+            aff[:,0:1],
+            torch.zeros_like(aff[:,0:1]),
+            aff[:,1:2],
+            aff[:,2:3]],
+            dim=1
+        ).reshape(-1,2,2)
+        laf = torch.cat([
+            aff2, torch.zeros_like(aff2[:,:, 0:1])
+        ],dim=2).unsqueeze(1)
     
     ls = KF.get_laf_scale(laf)
-    laf2 = KF.scale_laf(KF.make_upright(laf), 1./ls).squeeze(1)
+    laf2 = KF.scale_laf(
+        KF.make_upright(laf), 1./ls
+    ).squeeze(1)
     return laf2[:,:2,:2].detach().cpu().numpy()
 
 
@@ -47,13 +56,18 @@ def orinet_radians(inp, orinet):
 def estimate_orientation(kpts, img, As, orinet, dev = torch.device('cpu')):
     orinet = orinet.to(dev)
     orinet.eval()
-    patches = np.array(extract_patches((kpts,As),
-                                       cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),
-                                       32, 12., 'cv2+A')).astype(np.float32)
+    patches = np.array(
+        extract_patches(
+            (kpts,As),
+            cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), 32, 12., 'cv2+A'
+        )
+    ).astype(np.float32)
     bs = 128
     aff = np.zeros((len(patches)))
     for i in range(0, len(patches), bs):
-        data_a = torch.from_numpy(patches[i:min(i + bs, len(patches)),  :, :]).unsqueeze(1).to(dev)
+        data_a = torch.from_numpy(
+            patches[i:min(i + bs, len(patches)),  :, :]
+        ).unsqueeze(1).to(dev)
         with torch.no_grad():
             out_a = orinet_radians(data_a, orinet)
             aff[i:i + bs] = out_a.cpu().detach().numpy()
@@ -64,13 +78,19 @@ def estimate_orientation(kpts, img, As, orinet, dev = torch.device('cpu')):
 def extract_descriptors(kpts, img, As, descnet, dev=torch.device('cpu')):
     descnet = descnet.to(dev)
     descnet.eval()
-    patches = np.array(extract_patches((kpts,As),
-                                       cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),
-                                       32, 12., 'cv2+A')).astype(np.float32)
+    patches = np.array(
+        extract_patches(
+            (kpts,As),
+            cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),
+            32, 12., 'cv2+A'
+        )
+    ).astype(np.float32)
     bs = 128
     desc = np.zeros((len(patches), 128))
     for i in range(0, len(patches), bs):
-        data_a = torch.from_numpy(patches[i:min(i + bs, len(patches)),  :, :]).unsqueeze(1).to(dev)
+        data_a = torch.from_numpy(
+            patches[i:min(i + bs, len(patches)),  :, :]
+        ).unsqueeze(1).to(dev)
         with torch.no_grad():
             out_a = descnet(data_a)
             desc[i:i + bs] = out_a.cpu().detach().numpy()
@@ -83,7 +103,12 @@ def extract_sift_keypoints_upright(img, n_feat = 5000):
     keypoints = sift.detect(img, None)
     response = np.array([kp.response for kp in keypoints])
     respSort = np.argsort(response)[::-1]
-    kpts = [cv2.KeyPoint(keypoints[i].pt[0], keypoints[i].pt[1], keypoints[i].size, 0) for i in respSort]
+    kpts = [
+        cv2.KeyPoint(
+            keypoints[i].pt[0], keypoints[i].pt[1], keypoints[i].size, 0
+        )
+        for i in respSort
+    ]
     kpts_unique = []
     for x in kpts:
         if x not in kpts_unique:
@@ -95,7 +120,10 @@ def match_snn(desc1, desc2, snn_th = 0.8):
     index_params = dict(algorithm=1, trees=4)
     search_params = dict(checks=128)  # or pass empty dictionary
     matcher = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = matcher.knnMatch(desc1.astype(np.float32), desc2.astype(np.float32), k=2)
+    matches = matcher.knnMatch(
+        desc1.astype(np.float32),
+        desc2.astype(np.float32), k=2
+    )
     good_matches = []
     for m,n in matches:
         if m.distance < snn_th * n.distance:
@@ -114,8 +142,13 @@ def detect_affnet_descriptors(img, nfeats = 5000, dev=torch.device('cpu')):
     kpts = extract_sift_keypoints_upright(img, nfeats)
     As = extimate_affine_shape(kpts, img, affnet, dev)
     ori = estimate_orientation(kpts, img, As, orinet, dev)
-    kpts_new = [cv2.KeyPoint(x.pt[0], x.pt[1], x.size, ang) for x, ang in zip(kpts,ori)]
-    descs = extract_descriptors(kpts_new, img, As, hardnet, dev)
+    kpts_new = [
+        cv2.KeyPoint(x.pt[0], x.pt[1], x.size, ang)
+        for x, ang in zip(kpts,ori)
+    ]
+    descs = extract_descriptors(
+        kpts_new, img, As, hardnet, dev
+    )
     print('number of descs: ', len(descs))
     return kpts_new, descs, As
 
--- pose_estimation_class.py
+++ pose_estimation_class.py
@@ -1,25 +1,23 @@
-
 import numpy as np
 import os
 import cv2
 import json 
-import time
 import torch
-import argparse
-import kornia as K
-import pydegensac
-import kornia.feature as KF
-import matplotlib.pyplot as plt
-from kornia.feature import laf_from_center_scale_ori as get_laf
-from copy import deepcopy
-from extract_patches.core import extract_patches
-
 from affnet_descriptor import detect_affnet_descriptors, match_snn
 
-class FeatureDescriptor:
-    pass
 
 def mae(predictions, targets):
+    '''
+    Calculate Mean Average Error
+    Parameters
+    ----------
+    predictions
+    targets
+
+    Returns
+    -------
+
+    '''
     return np.mean(np.abs(predictions - targets))
 
 def convertQuaternionToMatrix(w, x, y, z):
@@ -34,7 +32,9 @@ def convertQuaternionToMatrix(w, x, y, z):
        2.0 * y * z + 2.0 * w * x,
        1.0 - 2.0 * x * x - 2.0 * y * y
     ], dtype=np.float32)
-    return rotation_v.reshape((3, 3)) # np.array([[fx, 0, cx],
+    return rotation_v.reshape(
+        (3, 3)
+    ) # np.array([[fx, 0, cx],
 
 class PoseEstimation:
     '''
@@ -43,13 +43,13 @@ class PoseEstimation:
     Methods
     -------
     '''
-    def __init__(self): #, query_path, views_path):
+    def __init__(self):
         '''
         Initialize data containers
         '''
-        self.NFEATS = 5000 # 2000 # 
+        self.NFEATS = 5000
         self.RATIO = 0.45 # factor for resize images
-        self.F_CONF = 0.98 # 0.95
+        self.F_CONF = 0.98
         self.query_img = None
         self.query_pose = None
         self.view_pose = None
@@ -80,14 +80,14 @@ class PoseEstimation:
         width, height = self.query_img.shape[:2]
         dsize = (int(width * self.RATIO), int(height * self.RATIO))
         self.query_img = cv2.resize(self.query_img, dsize)
-        
-        #print("query image shape")
-        #print(self.query_img.shape)
-        
+
         # Load metatdata for query image
         query_dir_path = os.path.dirname(self.query_path)
         query_filename = os.path.basename(self.query_path).split('.')[0] + '.json'
-        query_json_file = os.path.join(query_dir_path, query_filename)
+        query_json_file = os.path.join(
+            query_dir_path,
+            query_filename
+        )
         print('Load query metadata')
         query_calib, query_pose = self.load_image_metadata(query_json_file)
         self.query_pose = query_pose
@@ -106,20 +106,16 @@ class PoseEstimation:
         print('Loading image metadata')
         json_data = json.load(open(json_path))
         calib_info = json_data["calibration"]
-        #print(calib_info)
         fx = float(calib_info["fx"])
         fy = float(calib_info["fy"])
         cx = float(calib_info["cx"])
         cy = float(calib_info["cy"])
         calib_dict = {'fx': fx, 'fy': fy, 'cx': cx, 'cy': cy }
-        #print(calib_dict)
         # load query pose
         pose_json = json_data['pose']
         origin = pose_json['origin']
         rotation = pose_json['rotation']
-        # gt_t = np.array(gt_origin) #.reshape(-1, 1)
         pose_dict = {'rotation': rotation, 'origin': origin}
-        #print(pose_dict)
         return (calib_dict, pose_dict)
 
     def load_view_images(self):
@@ -127,22 +123,30 @@ class PoseEstimation:
         Load view images from folder
         '''
         print('Loading view images...')
-        view_image_files = [f for f in os.listdir(self.views_path) if f.endswith('jpg')]
-        self.view_names = [f.split('.')[0] for f in os.listdir(self.views_path) if f.endswith('jpg')]
-        self.view_images = [cv2.imread(os.path.join(self.views_path, f)) for f in view_image_files]
-        # self.view_images = self.view_images[:3] #10 # 80] # [:15]
-        # print(self.view_names)
+        view_image_files = [
+            f for f in os.listdir(self.views_path)
+            if f.endswith('jpg')
+        ]
+        self.view_names = [
+            f.split('.')[0]
+            for f in os.listdir(self.views_path)
+            if f.endswith('jpg')
+        ]
+        self.view_images = [
+            cv2.imread(os.path.join(self.views_path, f))
+            for f in view_image_files
+        ]
 
     def find_match(self):
         '''
         Find the best match for query image among view images usinf AffNet feature descriptors
         '''
         dev = torch.device('cpu')
-
-        #print("query image shape")
-        #print(self.query_img.shape)
         
-        query_kp, query_des, As1 = detect_affnet_descriptors(self.query_img, self.NFEATS, dev)
+        query_kp, query_des, As1 = detect_affnet_descriptors(
+            self.query_img,
+            self.NFEATS, dev
+        )
         self.query_kp = query_kp
         self.query_des = query_des
         
@@ -155,11 +159,17 @@ class PoseEstimation:
             dsize = (int(width * self.RATIO), int(height * self.RATIO))
 
             view_img = cv2.resize(view_img, dsize)
-            view_kp, view_des, As2 = detect_affnet_descriptors(view_img, self.NFEATS, dev)
-            # print('view %s has descriptors: %s' % (i, len(view_kp)))
-            thresh = 0.4 
-            tentatives = match_snn(self.query_des, view_des, thresh) # 0.85)
-            # print('view %s has inliers: %s' % (i, len(tentatives)))
+            view_kp, view_des, As2 = detect_affnet_descriptors(
+                view_img,
+                self.NFEATS,
+                dev
+            )
+            thresh = 0.4
+            tentatives = match_snn(
+                self.query_des,
+                view_des,
+                thresh
+            )
             self.views_keypoints.append(view_kp)
             self.views_descriptors.append(view_des)
             self.views_inliers.append(tentatives)
@@ -173,21 +183,21 @@ class PoseEstimation:
                 max_inliers = self.view_inliers_numbers[i]
                 self.best_view_index = i
         
-        self.best_match_inliers_number = max_inliers 
-        #print('best_view_index: ', self.best_view_index)
-        #print('best view: ', self.view_names[self.best_view_index])
+        self.best_match_inliers_number = max_inliers
         self.best_view_kp = self.views_keypoints[self.best_view_index]
         # Read camera parameters for best view
-        view_json_file = os.path.join(self.views_path, '%s.json' % self.view_names[self.best_view_index]) # args.query.split('.')[0] + '.json'
-        #print('view_json_file: ', view_json_file)
-        # print(json_file)
-    
+        view_json_file = os.path.join(
+            self.views_path,
+            '%s.json' % self.view_names[self.best_view_index]
+        )
+
         view_calib, view_pose = self.load_image_metadata(view_json_file)
         self.view_pose = view_pose
-        self.K_v = np.array([[view_calib['fx'], 0, view_calib['cx']],
-                        [0, view_calib['fy'], view_calib['cy']],
-                        [0, 0, 1]], dtype=np.float32) 
-
+        self.K_v = np.array([
+            [view_calib['fx'], 0, view_calib['cx']],
+            [0, view_calib['fy'], view_calib['cy']],
+            [0, 0, 1]], dtype=np.float32
+        )
 
     def estimate_pose(self) -> (dict, int):
         '''
@@ -200,16 +210,26 @@ class PoseEstimation:
         # Load point correspondences
         pts1 = []
         pts2 = []
-        best_match_inliers = self.views_inliers[self.best_view_index]
+        best_match_inliers = self.views_inliers[
+            self.best_view_index
+        ]
         best_match_inliers = best_match_inliers[:100]
         for i, match in enumerate(best_match_inliers):
-            pts1.append(self.query_kp[match.queryIdx].pt)
-            pts2.append(self.best_view_kp[match.trainIdx].pt)
+            pts1.append(
+                self.query_kp[match.queryIdx].pt
+            )
+            pts2.append(
+                self.best_view_kp[match.trainIdx].pt
+            )
         pts1 = np.array(pts1)
         pts2 = np.array(pts2)
-        dist_coeffs = None 
-        thresh = 0.0005 
-        F, mask = cv2.findFundamentalMat(pts1, pts2, method=cv2.FM_LMEDS, confidence=self.F_CONF) 
+
+        F, mask = cv2.findFundamentalMat(
+            pts1,
+            pts2,
+            method=cv2.FM_LMEDS,
+            confidence=self.F_CONF
+        )
         E = self.K_v.T @ F @ self.K_q
         # Use SVD to recover pose
         w,u,vt = cv2.SVDecomp(np.mat(E))   
@@ -217,8 +237,13 @@ class PoseEstimation:
             u *= -1.0
         if np.linalg.det(vt) < 0:
             vt *= -1.0 
+        
         #Find R and T from Hartley & Zisserman
-        W=np.mat([[0,-1,0],[1,0,0],[0,0,1]],dtype=float)
+        W=np.mat([
+            [0,-1,0],
+            [1,0,0],
+            [0,0,1]
+        ],dtype=float)
         R_m = np.mat(u) * W * np.mat(vt)
         print('R_m')
         print(R_m)
@@ -234,14 +259,17 @@ class PoseEstimation:
         t_q = np.linalg.inv(R_m) @ t 
         print('t_q')
         print(t_q)
-        R_q = R @ R_m # best_pose['R']
+        R_q = R @ R_m
         # R_q = R @ best_pose['R']
         print('R_q')
         # print(R_q.shape)
         print(R_q)
         self.result_pose = {'R': R_q, 't': t_q}
         self.calculate_pose_error()
-        return (self.result_pose, self.best_match_inliers_number)
+        return (
+            self.result_pose,
+            self.best_match_inliers_number
+        )
     
     def calculate_pose_error(self):
         '''
@@ -257,5 +285,8 @@ class PoseEstimation:
         #print(gt_t)
         t_error = mae(gt_t, pred_t)
         print('pose error: ', t_error)
-        R_error = mae(gt_R, self.result_pose['R'])
+        R_error = mae(
+            gt_R,
+            self.result_pose['R']
+        )
         print('rotation error: ', R_error)
